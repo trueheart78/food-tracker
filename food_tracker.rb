d4a0d82f922de1fc6aa8e7bab78b364444230c5e
Josh Mills <josh@trueheart78.com>
2021-03-17 13:03:20 -0400

    Updated to support basic reading of files, and new endpoints

--- food_tracker.rb
+++ food_tracker.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# FoodTracker is a Sinatra-based application to display the proper kitchen-based-items.
 class FoodTracker < Sinatra::Base
   set :environment, Env.to_sym
 
@@ -7,19 +8,53 @@ class FoodTracker < Sinatra::Base
     if Env.force_ssl?(request)
       redirect request.url.sub('http', 'https'), 308
     else
-      @url = Env.host(request)
-      @image = [@url, 'images', 'hamburger.png'].join '/'
-      @gif = [@url, 'images', 'hamburger-rotating.gif'].join '/'
-      @domain = Env.domain(request)
+      @site = { url:    Env.host(request),
+                image:  image('hamburger.png', request: request),
+                domain: Env.domain(request)
+      }
     end
   end
 
   get '/' do
+    @image = image 'hamburger.png'
+
     erb :index
   end
 
+  get '/in-the-kitchen' do
+    # TODO: if cache is not up-to-date, redirect to '/caching'
+    @image = image 'hamburger.png'
+
+    files = Dir['data/*.txt'].sort
+    output = []
+    files.each do |file|
+      output << file
+      output += File.readlines(file).map(&:chomp)
+    end
+
+    data = output.reject { |o| o == '' }.map { |o| "<li>#{o} </li>\n" }
+    erb :kitchen do
+      "<ol>#{data.join}</ol>"
+    end
+  end
+
   get '/caching' do
+    @gif = image 'hamburger-rotating.gif'
+
     erb :caching
+  end
+
+  get '/caching-perform' do
+    # TODO: make sure this isn't already running
+    20.times { sleep 1 }
+
+    redirect '/'
+  end
+
+  get '/caching-flush' do
+    # TODO: flush the cache
+
+    redirect '/in-the-kitchen'
   end
 
   get '/api' do
@@ -33,5 +68,13 @@ class FoodTracker < Sinatra::Base
 
   post '/*' do
     404
+  end
+
+  # private
+
+  def image(name, request: nil)
+    return [Env.host(request), 'images', name].join('/') if request
+
+    ['/images', name].join '/'
   end
 end
