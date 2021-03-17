5e4670dd38c1a6ff0bd574e06b834b6e0d1d4464
Josh Mills <josh@trueheart78.com>
2021-03-17 11:23:37 -0400

    New caching endpoint

--- food_tracker.rb
+++ food_tracker.rb
@@ -2,13 +2,14 @@
 
 class FoodTracker < Sinatra::Base
   set :environment, Env.to_sym
-  
+
   before do
     if Env.force_ssl?(request)
       redirect request.url.sub('http', 'https'), 308
     else
       @url = Env.host(request)
       @image = [@url, 'images', 'hamburger.png'].join '/'
+      @gif = [@url, 'images', 'hamburger-rotating.gif'].join '/'
       @domain = Env.domain(request)
     end
   end
@@ -16,7 +17,11 @@ class FoodTracker < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
+  get '/caching' do
+    erb :caching
+  end
+
   get '/api' do
     json food: 'I love to eat it!'
   end
