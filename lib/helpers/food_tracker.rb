2ca08f6b7919233ab73ee20a60099cfda7c63778
Josh Mills <josh@trueheart78.com>
2021-03-21 00:50:10 -0400

    Bug fix in Helpers::FoodTracker

--- lib/helpers/food_tracker.rb
+++ lib/helpers/food_tracker.rb
@@ -5,13 +5,13 @@ module Helpers
     def site_icon
       'hamburger'
     end
-    
+
     def site_description
       'A personalized food tracker.'
     end
-    
+
     def site_settings(request)
-      { 
+      {
         url:         Env.host(request),
         image:       image("#{site_icon}.png", request: request),
         twitter:     twitter_settings,
@@ -21,7 +21,7 @@ module Helpers
         color:       default_settings[:color]
       }
     end
-    
+
     def twitter_settings
       {
         image_alt: site_icon,
@@ -29,36 +29,36 @@ module Helpers
         site:      Env.twitter_site
       }
     end
-    
+
     def default_settings
       {
         title: 'Food, Pls?',
         color: '#ffdb58'
       }
     end
-    
+
     def insert_touch_icons(site)
       site[:touch_icon] = 'apple-touch-icon.png'
       site[:precomposed_icon] = 'apple-touch-icon-procomposed.png'
-      
+
       unless site[:color] == default_settings[:color]
         color = site[:color].downcase.sub('#', '')
-        
+
         site[:touch_icon] = touch_icon(color) if public? touch_icon(color)
-        site[:precomposed_icon] = precomposed_icon(color) if public? precomposed_json(color)
+        site[:precomposed_icon] = precomposed_icon(color) if public? precomposed_icon(color)
       end
-      
+
       site
     end
-    
+
     def public?(file)
       File.exist? File.join('public', file)
     end
-    
+
     def touch_icon(color)
       "apple-touch-icon-#{color}.png"
     end
-    
+
     def precomposed_icon(color)
       "apple-touch-icon-precomposed-#{color}.png"
     end
@@ -68,11 +68,11 @@ module Helpers
 
       ['/images', name].join '/'
     end
-    
+
     def site_image
       image "#{site_icon}.png"
     end
-    
+
     def site_gif
       image "#{site_icon}-rotating.gif"
     end
