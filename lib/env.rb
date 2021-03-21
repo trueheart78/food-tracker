2c1744118559af937ebe5b99508837c5382514e1
Josh <trueheart78@users.noreply.github.com>
2021-03-20 20:34:50 -0400

    Env now supports Twitter-based environment vars.

--- lib/env.rb
+++ lib/env.rb
@@ -20,7 +20,31 @@ class Env
   def self.to_sym
     ENV['RACK_ENV'].to_sym
   end
+  
+  def self.twitter_handle?
+    return false if self.twitter_creator.empty?
+    
+    true
+  end
 
+  def self.twitter_creator
+    if ENV['TWITTER_CREATOR'] && !ENV['TWITTER_CREATOR'].empty?
+      return ENV['TWITTER_CREATOR'] if ENV['TWITTER_HANDLE'].start_with? '@'
+      return "@#{ENV['TWITTER_CREATOR']}"
+    end
+    
+    ''
+  end
+  
+  def self.twitter_site
+    if ENV['TWITTER_SITE'] && !ENV['TWITTER_SITE'].empty?
+      return ENV['TWITTER_SITE'] if ENV['TWITTER_SITE'].start_with? '@'
+      return "@#{ENV['TWITTER_SITE']}"
+    end
+    
+    self.twitter_creator
+  end
+  
   def self.host(request)
     request.base_url
   end
