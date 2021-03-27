0e3f13c208e3b30cf22a6457dda8635f0022aa6b
Josh <trueheart78@users.noreply.github.com>
2021-03-26 21:02:01 -0400

    Changed RAPP_ENV to APP_ENV

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-ENV['APP_ENV'] = 'development' unless ENV['RAPP_ENV']
+ENV['APP_ENV'] = 'development' unless ENV['APP_ENV']
 if ENV['APP_ENV'] != 'production'
   require 'dotenv'
   Dotenv.load ".env.#{ENV['APP_ENV']}", '.env.local', '.env'
