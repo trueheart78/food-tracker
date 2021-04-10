209603341efde3c9a77e902cce274217b0a325d5
Josh Mills <josh@trueheart78.com>
2021-04-10 15:53:26 -0400

    Refactored tasks/sort_items.rb

--- tasks/sort_items.rb
+++ tasks/sort_items.rb
@@ -3,16 +3,43 @@
 
 require_relative '../booster_pack'
 
-Dir['data/*.yaml'].sort.each do |filename|
-  print "=> Opening #{filename} "
-  data = YAML.load_file filename
+class SortData
+  class << self
+    def run!(directory: 'data')
+      puts "=> Sorting file contents in the [#{directory}] directory"
+      files = Dir["#{directory}/*.yaml"].sort
 
-  print '==> Sorting '
-  data[:types].sort!
-  data[:items].sort!
-  File.open(filename, 'w+') do |file|
-    print '==> Saving '
-    file.write data.to_yaml.sub("\n:items:", "\n\n:items:").gsub("\n-", "\n  -")
+      files.each { |filename| sort filename: filename }
+
+      puts '=> Complete!'
+    end
+
+    private
+
+    def sort(filename:)
+      print "==> Opening #{filename} "
+      data = YAML.load_file filename
+
+      print '==> Sorting '
+      data[:types].sort!
+      data[:items].sort!
+
+      save filename: filename, data: data
+
+      puts '==> Complete!'
+    end
+
+    def save(filename:, data:)
+      File.open(filename, 'w+') do |file|
+        print '==> Saving '
+        file.write yamlize(data: data)
+      end
+    end
+
+    def yamlize(data:)
+      data.to_yaml.sub("\n:items:", "\n\n:items:").gsub("\n-", "\n  -")
+    end
   end
-  puts '==> Complete!'
 end
+
+SortData.run!
