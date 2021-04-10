3d65c2ffa104b06abddc0045d9875ed2526a1bd0
Josh Mills <josh@trueheart78.com>
2021-04-10 14:04:39 -0400

    Remove Rollbar initializer

--- init/rollbar.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'rollbar/middleware/sinatra'
-
-Rollbar.configure do |config|
-  config.environment  = ENV['APP_ENV']
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-end
