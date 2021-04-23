8e0b21358807a01632182ee6e9421762e2b02d02
Josh Mills <josh@trueheart78.com>
2021-04-23 17:08:58 -0400

    More Site development

--- lib/site.rb
+++ lib/site.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
 class Site
-  attr_reader :url, :domain, :icon, :description
+  attr_accessor :title, :style
+  attr_reader   :url, :domain, :icon, :description
 
   def initialize(url:, domain:)
     @url         = url
     @domain      = domain
     @icon        = :hamburger
+    @title       = 'Food, Pls?'
     @description = 'A personalized food tracker.'
+    @style       = :default
+    @show_footer = true
+  end
+
+  def disable_footer
+    @show_footer = false
+  end
+
+  def footer?
+    @show_footer
   end
 end
--- spec/lib/site_spec.rb
+++ spec/lib/site_spec.rb
@@ -1,30 +1,53 @@
 # frozen_string_literal: true
 
 RSpec.describe Site, type: :model do
-  subject(:site) { described_class.new url: url, domain: domain }
-
-  let(:url)    { 'https://www.food-tracker.com' }
-  let(:domain) { url.sub 'https://', '' }
+  let(:default_site) { described_class.new url: url, domain: domain }
+  let(:url)          { 'https://www.food-tracker.com' }
+  let(:domain)       { url.sub 'https://', '' }
 
   context 'when initialized' do
-    it 'has the expected url' do
+    subject(:site) { described_class.new url: url, domain: domain }
+
+    it 'has the url' do
       expect(site.url).to eq url
     end
 
-    it 'has the expected domain' do
+    it 'has the domain' do
       expect(site.domain).to eq domain
     end
 
-    it 'has the expected icon' do
+    it 'has the icon' do
       expect(site.icon).to eq :hamburger
     end
 
-    it 'has the expected description' do
+    it 'has the default title' do
+      expect(site.title).to eq 'Food, Pls?'
+    end
+
+    it 'has the correct description' do
       expect(site.description).to eq 'A personalized food tracker.'
+    end
+
+    it 'has the default style' do
+      expect(site.style).to eq :default
+    end
+
+    it 'has the footer enabled' do
+      expect(site.footer?).to eq true
     end
   end
 
-  xit do
-    binding.pry
+  describe '#footer?' do
+    subject(:footer) { default_site.footer? }
+
+    context 'when first initialized' do
+      it { is_expected.to eq true }
+    end
+
+    context 'when disabled' do
+      before { default_site.disable_footer }
+
+      it { is_expected.to eq false }
+    end
   end
 end
