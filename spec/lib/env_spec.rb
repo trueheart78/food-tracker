2742ffb8d6f2e6db171c7ddeba1229ed525c5743
Josh Mills <josh@trueheart78.com>
2021-04-26 10:01:50 -0400

    Specs for Env.twitter_site

--- spec/lib/env_spec.rb
+++ spec/lib/env_spec.rb
@@ -118,4 +118,42 @@ RSpec.describe Env do
       end
     end
   end
+
+  describe '.twitter_site' do
+    before { set_env :TWITTER_CREATOR, twitter_creator }
+
+    let(:twitter_creator) { '@username' }
+
+    context 'when the environment variable is unset' do
+      before { set_env :TWITTER_SITE, nil }
+
+      it 'returns the twitter_creator' do
+        expect(described_class.twitter_site).to eq twitter_creator
+      end
+    end
+
+    context 'when the environment variable is empty' do
+      before { set_env :TWITTER_SITE, '' }
+
+      it 'returns the twitter_creator' do
+        expect(described_class.twitter_site).to eq twitter_creator
+      end
+    end
+
+    context 'when the environment variable does not have a leading @' do
+      before { set_env :TWITTER_SITE, :username }
+
+      it 'returns the username with a leading @' do
+        expect(described_class.twitter_site).to eq '@username'
+      end
+    end
+
+    context 'when the environment variable has a leading @' do
+      before { set_env :TWITTER_SITE, '@username' }
+
+      it 'returns the username' do
+        expect(described_class.twitter_site).to eq '@username'
+      end
+    end
+  end
 end
